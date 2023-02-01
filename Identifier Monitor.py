1532151b2c79405bf34486c0262a2da7edf2816f
Mohamed Nayif <mohamednayif@gmail.com>
2023-02-01 14:41:07 +0400

    new

--- Identifier Monitor.py
+++ Identifier Monitor.py
@@ -1,41 +1,46 @@
 # pyinstaller --onefile --console --name="MQTT Identifier Monitor" "Identifier Monitor.py" --specpath=build
-from MQTT import MQTT 
+from MQTT import MQTT
 import os
 import sys
 from time import sleep
 import platform
 import subprocess
 
+
 def identifier_monitor(Mqtt: MQTT):
-    Mqtt.subscribe_topic = input("Topic to Subscribe: ")
-    Mqtt.identifier_to_monitor = input("Identifier to Monitor: ")
+    Mqtt.subscribe_topic = input("Topic to Subscribe (Optional) : ")
+    Mqtt.identifier_to_monitor = input("Identifier to Monitor (Required)   : ")
     if Mqtt.identifier_to_monitor is not "":
         try:
             print("\nPress Ctrl+C to stop\n")
             Mqtt.connect()
             Mqtt.client.loop_forever()
         except KeyboardInterrupt:
-            Mqtt.disconnect()            
+            pass
         except Exception as e:
-            print(f"Identifier Monitor ::: Error ::: {e}")  
-        print("Application will be closed in 5 seconds.")
-        sleep(5)
-        sys.exit()
+            print(f"Identifier Monitor ::: Error ::: {e}")
+        finally:
+            Mqtt.disconnect()
+            print("Application will be closed in 5 seconds.")
+            sleep(5)
+            sys.exit()
     else:
-        clear_terminal() 
-        print("No Identifier to Monitor. Please try again.")  
+        clear_terminal()
+        print("No Identifier to Monitor. Please try again.\n")
         identifier_monitor(Mqtt)
 
+
 def clear_terminal():
-    if platform.system() == 'Windows':
+    if platform.system() == "Windows":
         subprocess.run(["C:\\Windows\\System32\\cmd.exe", "/c", "cls"], check=True)
     else:
         subprocess.run(["/usr/bin/clear"], check=True)
 
+
 def main():
-    Mqtt = MQTT(on_connect_funcs=['subscribe'], on_message_funcs=['identifier_monitor'])     
-    identifier_monitor(Mqtt) 
-    
-    
+    Mqtt = MQTT(on_connect_funcs=["subscribe"], on_message_funcs=["identifier_monitor"])
+    identifier_monitor(Mqtt)
+
+
 if __name__ == "__main__":
     main()
